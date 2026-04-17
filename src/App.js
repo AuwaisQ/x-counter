2283494d4ae6948dcfaf80815bfa6db0c30c635f
auwaisqureshi <auwaisqureshi@gmail.com>
2026-04-17 17:15:47 +0530

    complete-1

--- src/App.js
+++ src/App.js
@@ -7,10 +7,14 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Counter App</h1>
-      <h3>Count:- {count}</h3>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <div className="counter-card">
+        <h1>Counter App</h1>
+        <h3>Count: {count}</h3>
+        <div className="button-group">
+          <button onClick={() => setCount(count + 1)}>Increment</button>
+          <button onClick={() => setCount(count - 1)}>Decrement</button>
+        </div>
+      </div>
     </div>
   );
 }
